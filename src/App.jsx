6442dde3447fc144eb45973490d448d6e9d0e7c8
unknown <isaiaspadros2@gmail.com>
2023-07-11 15:11:30 -0300

    Primer commit general

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Titulo from './componentes/Titulo';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  
+
+  return (
+   <Container>
+   <Titulo></Titulo>
+   </Container>
+    
+    )
+}
+
+export default App
---
+++ src/componentes/Titulo.jsx
@@ -0,0 +1,12 @@
+
+const Titulo = () => {
+    return (
+        <section className="text-center">
+            <h1 className="display-4">Primer titulo</h1>
+            <h2 className="display-6">Subtitulo</h2>
+            <hr />
+        </section>
+    );
+};
+
+export default Titulo;
